a347bf156dd41f8df834cb5f90ce49f752e26c8a
Kim-GeonHo <rjsgh7943@gist.ac.kr>
2023-09-19 20:45:43 +0900

    [Gold V] Title: 신기한 소수, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.cc
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.cc
@@ -3,11 +3,15 @@ int n;
 
 using namespace std;
 bool check(int k) {
+    if (k == 2) {
+        return true;
+    }
+
     if (k < 2) {
         return false;
     }
 
-    for (int i=2; i<sqrt(k)+1; i++) {
+    for (int i=2; i<=sqrt(k); i++) {
         if (k%i == 0) {
             return false;
         }
@@ -17,16 +21,12 @@ bool check(int k) {
 }
 
 void dfs(int num) {
-    if (to_string(num).length() == n) {
+    if (num != 0 && to_string(num).length() == n) {
         cout << num << "\n";
         return;
     }
 
     for (int i=1; i<10; i++) {
-        if (i%2 == 0) {
-            continue;
-        }
-
         if (check(num*10 + i)) {
             dfs(num*10 + i);
         }
@@ -38,10 +38,7 @@ void dfs(int num) {
 int main() {
     cin >> n;
 
-    dfs(2);
-    dfs(3);
-    dfs(5);
-    dfs(7);
+    dfs(0);
 
     return 0;
 }
